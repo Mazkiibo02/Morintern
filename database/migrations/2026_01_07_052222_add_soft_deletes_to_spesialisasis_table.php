5df4d47bea4cfb70bf647dbf616e9d16d4b7b8e0
Hafidz Rahmatullah <117551369+Mazkiibo02@users.noreply.github.com>
2026-01-07 12:45:02 +0700

    perbaikan 7 januari 2026

--- database/migrations/2026_01_07_052222_add_soft_deletes_to_spesialisasis_table.php
+++ database/migrations/2026_01_07_052222_add_soft_deletes_to_spesialisasis_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('spesialisasis', function (Blueprint $table) {
-            $table->id();
-            $table->string('nama_spesialisasi');
-            $table->text('deskripsi')->nullable();
-            $table->timestamps();
+        Schema::table('spesialisasis', function (Blueprint $table) {
+            $table->softDeletes();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('spesialisasis');
+        Schema::table('spesialisasis', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
 };
